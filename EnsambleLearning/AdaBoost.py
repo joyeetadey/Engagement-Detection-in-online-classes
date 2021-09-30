ada50480fc5ca5b5345d7703aba06e7df4c6bd95
Joyeeta Dey <60482666+joyeetadey@users.noreply.github.com>
2021-09-30 23:12:24 +0530

    Add files via upload

---
+++ EnsambleLearning/AdaBoost.py
@@ -0,0 +1,57 @@
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+
+import csv
+
+data=open('finalV3.csv')
+csvreader=csv.reader(data)
+header=[]
+header=next(csvreader)
+rows=[]
+for row in csvreader:
+    rows.append(row)
+
+for x in rows:
+    if "" in x:
+        rows.remove(x)
+
+X=[x[:3] for x in rows]
+y=[x[3] for x in rows]
+for x in X:
+    x[0]=int(x[0])
+    if x[1]=="Active":
+        x[1]=1
+    else:
+        x[1]=0
+    if x[2]=="Frustated":
+        x[2]=0
+    elif x[2]=="Engaged":
+        x[2]=1
+    elif x[2]=="Bored":
+        x[2]=2
+    else:
+        x[2]=3
+avg_acc=0
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+
+clf=AdaBoostClassifier(n_estimators=100)
+clf.fit(X_train,y_train)
+
+d=clf.predict(X_test)
+
+print(classification_report(y_test,d))
+
+tree_para={'n_estimators':[200,300,50,150,250],}
+grid = GridSearchCV(AdaBoostClassifier(), tree_para, cv=5)
+  
+# fitting the model for grid search
+grid.fit(X_train, y_train)
+
+grid_predictions=grid.predict(X_test)
+
+print(classification_report(y_test, grid_predictions))
+print(grid.best_params_)
---
+++ EnsambleLearning/RandomForest.py
@@ -0,0 +1,46 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+import csv
+
+data=open('finalV3.csv')
+csvreader=csv.reader(data)
+header=[]
+header=next(csvreader)
+rows=[]
+for row in csvreader:
+    rows.append(row)
+
+for x in rows:
+    if "" in x:
+        rows.remove(x)
+
+X=[x[:3] for x in rows]
+y=[x[3] for x in rows]
+for x in X:
+    x[0]=int(x[0])
+    if x[1]=="Active":
+        x[1]=1
+    else:
+        x[1]=0
+    if x[2]=="Frustated":
+        x[2]=0
+    elif x[2]=="Engaged":
+        x[2]=1
+    elif x[2]=="Bored":
+        x[2]=2
+    else:
+        x[2]=3
+avg_acc=0
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
+
+clf=RandomForestClassifier(n_estimators=100)
+clf.fit(X_train,y_train)
+
+d=clf.predict(X_test)
+
+print(classification_report(y_test,d))
+#avg_acc+=sum([1 if d[i]==y_test[i] else 0 for i in range(len(d))])/len(d)
+#print("Average Accuracy for 10 tests:",avg_acc/10)
